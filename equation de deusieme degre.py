540793aba9adf7c0230395668f738f0057e741dc
kossay chemingui <110374361+kossaych@users.noreply.github.com>
2022-09-26 19:04:52 +0100

    Update equation de deusieme degre.py

--- equation de deusieme degre.py
+++ equation de deusieme degre.py
@@ -1,26 +1,11 @@
-from math import*
-e=input("donner l'equation de forme ax+bx+c")
-e1=e[5]
-plus="+"
-moin="-"
-pos1=e.find("x")
-pos2=e.find("+")
-pos3=e.find("x",pos1+1)
-pos4=e.find("+",pos2+1)
-a=e[0:pos1]
-b=e[pos2:pos3]
-c=e[pos3+1:]
-a=float(a)
-b=float(b)
-c=float(c)
-print(a)
-print(b)
-print(c)
-print(e1)
-delta=(b**2)-4*a*c
-print(delta)
-if e1==plus:
-    
+    from math import*
+    e=input("donner l'equation de forme ax+bx+c")
+    a=int(input("a :"))
+    b=int(input("b :"))
+    c=int(input("c :"))
+    print("l'equation:"a,"x+",b,"x²+",c)
+    delta=(b**2)-4*a*c
+    print("delta :",delta)
     if delta<0 :
             print("n'a pas des solition dans R")
     elif delta==0:
@@ -31,22 +16,6 @@ if e1==plus:
         print("S dans R[",s1,"/",2*a,",",s2,"/",2*a,"]")
     else :
         print("equation invalide")
-elif e1==moin :
-     if delta<0 :
-        print("n'a pas des solition dans R")
-     elif delta==0:
-        print("s dans r =",-b/2*a)
-     elif delta>0 :
-        s1=-b+sqrt(delta)
-        s2=-b-sqrt(delta)
-        print("S dans R[",s1,"/",2*a,",",s2,"/",2*a,"]")
-    
-else :
-    print('invalide')
-    
-
-
-    
 
 
 
